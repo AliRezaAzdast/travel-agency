1f4175ae2cceaf1d4a4142b3ff5473e101eff0b7
ali reza azdast <azdast47@gmail.com>
2025-04-23 22:13:07 +0330

    made whyus part responsive

--- components/WhyUs.jsx
+++ components/WhyUs.jsx
@@ -4,7 +4,7 @@ const WhyUs = () => {
   return (
     <section className="mt-20">
       <div className="my-container">
-        <div className="w-full h-130 my-text"  style={{ backgroundImage: "url(./img/Frame.png" }}>
+        <div className="w-full lg:h-130 h-auto my-text"  style={{ backgroundImage: "url(./img/Frame.png" }}>
           {/* title */}
           <div className="flex justify-between items-center">
             <div className="flex gap-1 items-center">
@@ -19,18 +19,18 @@ const WhyUs = () => {
             </div>
           </div>
           {/* body */}
-          <div className="mt-8 bg-primary flex items-center gap-6 dark:border dark:border-[#FBFBFB]/25 dark:bg-[#1D1D1D] justify-between p-10 rounded-4xl">
-            <div className="bg-[#7D8BFF] dark:bg-[#4A4A4A] flex flex-col flex-1/3 items-center gap-y-5 relative rounded-4xl p-5 after:bg-[#7D8BFF] dark:after:bg-[#4A4A4A] after:rounded-full after:w-20 after:h-20 after:absolute after:-top-3 after:left-[42%]">
+          <div className="mt-8 bg-primary flex lg:flex-row flex-col items-center gap-6 dark:border dark:border-[#FBFBFB]/25 dark:bg-[#1D1D1D] justify-between p-10 rounded-4xl">
+            <div className="bg-[#7D8BFF] dark:bg-[#4A4A4A] flex flex-col w-[365px] items-center gap-y-5 relative rounded-4xl p-5 after:bg-[#7D8BFF] dark:after:bg-[#4A4A4A] after:rounded-full after:w-20 after:h-20 after:absolute after:-top-3 after:left-[42%]">
                 <img src="./img/icons/whytravel/clock.png" className="w-14 h-14 bg-primary dark:bg-[#1E1E1E] p-2 rounded-full z-10" alt="" />
                 <h4 className="text-white text-[28px] font-semibold text-center">پشتیبانی 24 ساعته</h4>
                 <p className="text-white text-lg text-center w-[240px]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
             </div>
-            <div className="bg-[#7D8BFF] dark:bg-[#4A4A4A] flex flex-col flex-1/3 items-center gap-y-5 relative rounded-4xl p-5 after:bg-[#7D8BFF] dark:after:bg-[#4A4A4A] after:rounded-full after:w-20 after:h-20 after:absolute after:-top-3 after:left-[42%]">
+            <div className="bg-[#7D8BFF] dark:bg-[#4A4A4A] flex flex-col w-[365px] items-center gap-y-5 relative rounded-4xl p-5 after:bg-[#7D8BFF] dark:after:bg-[#4A4A4A] after:rounded-full after:w-20 after:h-20 after:absolute after:-top-3 after:left-[42%]">
                 <img src="./img/icons/whytravel/dollar.png" className="w-14 h-14 bg-primary dark:bg-[#1E1E1E] p-2 rounded-full z-10" alt="" />
                 <h4 className="text-white text-[28px] font-semibold text-center ">پشتیبانی 24 ساعته</h4>
                 <p className="text-white text-lg text-center w-[240px]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
             </div>
-            <div className="bg-[#7D8BFF] dark:bg-[#4A4A4A] flex flex-col flex-1/3 items-center gap-y-5 relative rounded-4xl p-5 after:bg-[#7D8BFF] dark:after:bg-[#4A4A4A] after:rounded-full after:w-20 after:h-20 after:absolute after:-top-3 after:left-[42%]">
+            <div className="bg-[#7D8BFF] dark:bg-[#4A4A4A] flex flex-col w-[365px] items-center gap-y-5 relative rounded-4xl p-5 after:bg-[#7D8BFF] dark:after:bg-[#4A4A4A] after:rounded-full after:w-20 after:h-20 after:absolute after:-top-3 after:left-[42%]">
                 <img src="./img/icons/whytravel/plain-white.png" className="w-14 h-14 bg-primary dark:bg-[#1E1E1E] p-2 rounded-full z-10" alt="" />
                 <h4 className="text-white text-[28px] font-semibold text-center ">پشتیبانی 24 ساعته</h4>
                 <p className="text-white text-lg text-center w-[240px]">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. </p>
